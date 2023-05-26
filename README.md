1fc6601a780e9275cd1941c95bc7c116025617fe
Téo Calvo <teocalvo2@gmail.com>
2023-05-26 11:19:08 -0300

    readMe

--- README.md
+++ README.md
@@ -4,3 +4,35 @@ Exercício para nível iniciante ou intermediário de Python
 A ideia deste exercício é apresentar o conceito de simulação estatística para cálcular a área de uma circunferência gerando números aleatórios a partir de uma distribuição de probabilidade Uniforme.
 
 ## Setup
+
+Para executar os códigos é necessário instalar algumas bibliotecas, como: `numpy`, `pandas`, `matplotlib`. Para isso, basta executar o código abaixo com seu ambiente Python ativo:
+
+```bash
+$ pip install -r requirements.txt
+```
+
+## Organização do código
+
+```
+circle-area
+├── area_circulo.py
+├── area_cli.py
+├── LICENSE
+├── README.md
+└── requirements.txt
+```
+
+`area_circulo.py` -> foi criado em live e sua gravação se encontra no canal do [CodeShow](colocar o link aqui).
+
+`area_cli.py` -> Mesmo em live, utilizando os conceitos apresensatos, geramos um novo arquivo que pode ser utilizado para gerar diferentes cenários do mesmo problema. Isto é, circunferências e quantidade de amostras diferentes. Para brincar com este script, faça uso se seus parâmetros:
+
+```bash
+$ python area_cli.py --help
+usage: area_cli.py [-h] [--diametro DIAMETRO] [--samples SAMPLES] [--plot]
+
+options:
+  -h, --help            show this help message and exit
+  --diametro DIAMETRO, -d DIAMETRO
+  --samples SAMPLES, -s SAMPLES
+  --plot, -p
+```
--- area_cli.py
+++ area_cli.py
@@ -7,45 +7,64 @@ import matplotlib.pyplot as plt
 
 import argparse
 
+
 def dist(row, origem):
-    p1 = np.array([row['x'], row['y']])
+    p1 = np.array([row["x"], row["y"]])
     p2 = np.array(origem)
-    return np.linalg.norm(p1-p2)
+    return np.linalg.norm(p1 - p2)
 
 
 def plot(df):
-    df[df['flag_circunferencia']].plot.scatter(x='x', y='y')
+    df[df["flag_circunferencia"]].plot.scatter(x="x", y="y")
     plt.show()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--diametro", '-d', default=10, type=int)
-    parser.add_argument("--samples", '-s', default=100, type=int)
-    parser.add_argument("--plot", '-p', action='store_true')
+    parser.add_argument(
+        "--diametro",
+        "-d",
+        default=10,
+        type=int,
+        help="Tamanho do diâmetro da circunferência.",
+    )
+    parser.add_argument(
+        "--samples",
+        "-s",
+        default=100,
+        type=int,
+        help="Quantidade de amostras criadas para inferir a área",
+    )
+    parser.add_argument(
+        "--plot",
+        "-p",
+        action="store_true",
+        help="Quando invocado, realiza o plot do gráfico da circunferência",
+    )
     args = parser.parse_args()
-    
+
     n = args.samples
     diametro = args.diametro
     raio = diametro / 2
-    origem = diametro/2, diametro/2
+    origem = diametro / 2, diametro / 2
 
     points = np.random.uniform(low=0, high=10, size=[n, 2])
 
-    df = pd.DataFrame(points, columns=['x', 'y'])
-    df['dist_origem'] = df.apply(dist, args=(origem,), axis=1)
-    df['flag_circunferencia'] = df['dist_origem'] <= raio
+    df = pd.DataFrame(points, columns=["x", "y"])
+    df["dist_origem"] = df.apply(dist, args=(origem,), axis=1)
+    df["flag_circunferencia"] = df["dist_origem"] <= raio
 
     area_quadrado = diametro * diametro
-    area_circunferencia_aprox = area_quadrado * df['flag_circunferencia'].mean()
+    area_circunferencia_aprox = area_quadrado * df["flag_circunferencia"].mean()
 
     print(f"Tamanho da amostra: {n}")
     print("Área Quadrado (Exata):", area_quadrado)
     print("Área Circunferência (Aprox.):", area_circunferencia_aprox)
-    print("Área Circunferência (Exata):", np.pi * (raio ** 2))
+    print("Área Circunferência (Exata):", np.pi * (raio**2))
 
     if args.plot:
         plot(df)
+
 
 if __name__ == "__main__":
     main()
